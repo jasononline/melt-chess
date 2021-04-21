fe9fefdb9b27760796f85fd5210a052250a44b8c
Lucas Weinknecht <lucas.weinknecht@student.uni-luebeck.de>
2021-04-21 16:44:51 +0200

    Add skeleton for MoveGenerator class

---
+++ src/main/java/chess/model/MoveGenerator.java
@@ -0,0 +1,69 @@
+package chess.model;
+
+import java.util.ArrayList;
+
+/**
+ * Encapsulates the rules for moving pieces on the chess board
+ * and offers functions for finding and validating moves.
+ */
+public class MoveGenerator {
+
+
+    /**
+     * Generate a list of all possible moves
+     * @param board the current state of the game
+     */
+    public static ArrayList<Move> generateMoves(Board board) {
+        // TODO write tests
+        // TODO write function
+        return null;
+    }
+
+
+    /**
+     * Generate a list of all possible moves for pieces of a given color
+     * @param board the current state of the game
+     * @param color pieces color to find moves for
+     */
+    public static ArrayList<Move> generateMovesFor(Board board, int color) {
+        // TODO write tests
+        // TODO write function
+        return null;
+    }
+
+
+    /**
+     * Generate a list of all possible moves for pieces of the current color
+     * @param board the current state of the game
+     */
+    public static ArrayList<Move> generateNextMoves(Board board) {
+        // TODO write tests
+        // TODO write function
+        return null;
+    }
+
+
+    /**
+     * Returns true if the move is legal according to the rules of chess
+     * @param board the current state of the game
+     * @param move the move to check
+     */
+    public static ArrayList<Move> isValid(Board board, Move move) {
+        // TODO write tests
+        // TODO write function
+        return null;
+    }
+
+
+    /**
+     * Generate a list of all possible moves starting on a given square
+     * @param board the current state of the game
+     * @param startSquare the starting square
+     */
+    public static ArrayList<Move> generateMovesFrom(Board board, int startSquare) {
+        // TODO write tests
+        // TODO write function
+        return null;
+    }
+
+}
