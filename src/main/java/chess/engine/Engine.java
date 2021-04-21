b8098e7f960cb35ce25e07ff26a7896c964ee7d9
Lucas Weinknecht <lucas.weinknecht@student.uni-luebeck.de>
2021-04-21 16:57:32 +0200

    Add skeleton for Engine class

---
+++ src/main/java/chess/engine/Engine.java
@@ -0,0 +1,20 @@
+package chess.engine;
+
+import chess.model.*;
+
+/**
+ * Engine class for choosing next moves
+ */
+public class Engine {
+
+
+    /**
+     * Returns a next move, given a board position
+     * @param board the current board position
+     */
+    public Move getNextMove(Board board){
+        // TODO write tests
+        // TODO write function
+        return null;
+    }
+}
