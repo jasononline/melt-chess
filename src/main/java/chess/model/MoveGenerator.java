bfcd35b866103a6cbca5ce700df14c0b1a95d176
Lucas Weinknecht <lucas.weinknecht@student.uni-luebeck.de>
2021-04-27 02:43:20 +0200

    Add MoveGeneratorPawn.java Refactor move generation of the pawn

--- src/main/java/chess/model/MoveGenerator.java
+++ src/main/java/chess/model/MoveGenerator.java
@@ -58,45 +58,7 @@ public class MoveGenerator {
      * @return ArrayList of Move objects
      */
     public List<Move> generatePawnMoves(int startSquare) {
-
-        List<Move> generatedMoves = new ArrayList<>();
-        int direction = teamColor == Piece.Black ? DOWN : UP;
-
-        // move forward if possible
-        int forwardPosition = startSquare + direction;
-        if (!Coordinate.isUpMost(forwardPosition) && !Coordinate.isDownMost(forwardPosition)
-                && board.getPieceAt(forwardPosition) == Piece.None) {
-            generatedMoves.add(new Move(startSquare, forwardPosition));
-        }
-
-        // if moving forward lead to the last square in the file, promoting the piece is possible
-        if (board.getPieceAt(forwardPosition) == Piece.None
-                && (Coordinate.isUpMost(forwardPosition) || Coordinate.isDownMost(forwardPosition))) {
-            generatedMoves.add(new Move(startSquare, forwardPosition, Move.PromoteToQueen));
-            generatedMoves.add(new Move(startSquare, forwardPosition, Move.PromoteToKnight));
-            generatedMoves.add(new Move(startSquare, forwardPosition, Move.PromoteToBishop));
-            generatedMoves.add(new Move(startSquare, forwardPosition, Move.PromoteToRook));
-        }
-
-        // if still in starting row, move two squares forward
-        forwardPosition = startSquare + 2 * direction;
-        int rank = Coordinate.fromIndex(startSquare)[1];
-        if ((rank == 1 && direction == DOWN || rank == 6 && direction == UP)
-                && board.getPieceAt(forwardPosition) == Piece.None) {
-            generatedMoves.add(new Move(startSquare, forwardPosition, Move.PawnTwoForward));
-        }
-
-        // if possible, capture diagonal pieces
-        for (int diagonalPosition : new int[]{startSquare+direction+LEFT, startSquare+direction+RIGHT}) {
-            if (!Coordinate.isOnBorder(startSquare)) {
-                if (Piece.isColor(board.getPieceAt(diagonalPosition), opponentColor))
-                    generatedMoves.add(new Move(startSquare, diagonalPosition));
-                if (board.getEnPassantSquare() == diagonalPosition)
-                    generatedMoves.add(new Move(startSquare, diagonalPosition, Move.EnPassantCapture));
-            }
-        }
-
-        return generatedMoves;
+        return MoveGeneratorPawn.generatePawnMoves(board, startSquare);
     }
 
 
@@ -374,7 +336,7 @@ public class MoveGenerator {
     public List<Move> generateMovesStartingAt(int startSquare) {
         switch (Piece.getType(board.getPieceAt(startSquare))) {
             case Piece.Pawn:
-                return generatePawnMoves(startSquare);
+                return MoveGeneratorPawn.generatePawnMoves(board, startSquare);
             case Piece.Queen:
                 return generateQueenMoves(startSquare);
             case Piece.Bishop:
