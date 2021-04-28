1996e993ba8037978d800ac001b076663a04e55a
t.fischer <t.fischer@student.uni-luebeck.de>
2021-04-28 21:28:58 +0200

    Give some structure to Cli.java, adding TODOS

--- src/main/java/chess/cli/Cli.java
+++ src/main/java/chess/cli/Cli.java
@@ -1,11 +1,17 @@
 package chess.cli;
 
 import chess.model.*;
+import java.util.Scanner;
 
 /**
  * Starting point of the command line interface
  */
 public class Cli {
+    private static Game game = new Game();
+    private static boolean runningPvP = false;
+    private static boolean runningPvPC = false; // for Iteration2
+    private static boolean runningSimple = false; // for Test, maybe implement this differently?
+
     /**
      * The entry point of the CLI application.
      *
@@ -13,6 +19,8 @@ public class Cli {
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
+        // TODO (Miklos) write function
+        runGame();
     }
 
 
@@ -32,7 +40,52 @@ public class Cli {
      * Start a new game loop
      */
     public static void runGame() {
-        // no automated tests needed
-        // TODO write function
+        String input = "";
+        while (runningPvP) {
+            System.out.println(game.getCurrentPosition().toString());
+            input = getUserInput();
+            if (testUserInputSyntax(input)) {
+                performAction(input);
+            }
+        }
+    }
+
+    /**
+     * Perform the Action according to
+     * @param userInput
+     */
+    public static void performAction(String userInput) {
+        // TODO (Eva) implement function
+        assert true; // delete this line as soon as there is real code in this method
+    }
+
+    /**
+     * Test if the user input will have a defined effect
+     * @param userInput The user Input
+     * @return whether the user input will have a defined effect
+     */
+    public static boolean testUserInputSyntax(String userInput) {
+        // TODO (Miklos) implement function
+        return true;
+    }
+
+    /**
+     * Request input from the User
+     * @param messageToUser
+     * @return The user input as a String
+     */
+    public static String getUserInput(String messageToUser) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println(messageToUser);
+        return scan.next();
+    }
+
+    /**
+     * Request input from the User
+     * @return The user input as a String
+     */
+    public static String getUserInput() {
+        Scanner scan = new Scanner(System.in);
+        return scan.next();
     }
 }
