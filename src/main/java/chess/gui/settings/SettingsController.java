ffa9a54cb571e3260e361b181c5dc9f511d07245
Lucas <lucas.weinknecht@student.uni-luebeck.de>
2021-05-28 13:05:02 +0200

    Set controller class and onAction for the save Button in settings fxml Add corresponding dummy method handleBtnSaveOnAction

--- src/main/java/chess/gui/settings/SettingsController.java
+++ src/main/java/chess/gui/settings/SettingsController.java
@@ -1,7 +1,15 @@
 package chess.gui.settings;
 
+import javafx.fxml.FXML;
+
 /**
  * Controls the behaviour and actions of the UI elements in the settings scene
  */
 public class SettingsController {
+
+    @FXML
+    private void handleBtnSaveOnAction() {
+        // TODO set all new values in the settings model
+        System.out.println("User clicked on Save Button");
+    }
 }
