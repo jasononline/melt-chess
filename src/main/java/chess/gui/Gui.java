f58554db174f3d4e06d6c65c6d0d1f25a6134cad
Eva Norkunas <eva.norkunas@student.uni-luebeck.de>
2021-06-02 16:43:33 +0200

    View Update 1.0

--- src/main/java/chess/gui/Gui.java
+++ src/main/java/chess/gui/Gui.java
@@ -31,7 +31,7 @@ public class Gui extends Application {
         primaryStage.show();
 
          */
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("settings.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MenuView.fxml"));
         Parent root = null;
         try {
             root = (Parent)fxmlLoader.load();
