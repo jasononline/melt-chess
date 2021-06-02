d722883c4a0d9451d978ffc08e6b1cbc06bd173e
Miklos Libak <miklos.libak@student.uni-luebeck.de>
2021-06-02 20:00:46 +0200

    Add missing Javadoc

--- src/main/java/chess/model/MoveGeneratorDirectional.java
+++ src/main/java/chess/model/MoveGeneratorDirectional.java
@@ -1,6 +1,5 @@
 package chess.model;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,93 +8,89 @@ import java.util.List;
  */
 public class MoveGeneratorDirectional {
 
+	/**
+	 * Generates moves across the current file and rank
+	 * 
+	 * @param board       current game board
+	 * @param startSquare the position of the piece
+	 * @return ArrayList of Move objects
+	 */
+	public static List<Move> generateAcrossMoves(Board board, int startSquare) {
+		List<Integer> directions = generateStartingDirectionsAcross(startSquare);
+		return generateDirectionalMoves(board, startSquare, directions);
+	}
 
-    /**
-     * Generates moves across the current file and rank
-     * @param startSquare the position of the piece
-     * @return ArrayList of Move objects
-     */
-    public static List<Move> generateAcrossMoves(Board board, int startSquare) {
-        List<Integer> directions = generateStartingDirectionsAcross(startSquare);
-        return generateDirectionalMoves(board, startSquare, directions);
-    }
+	/**
+	 * Generate diagonal moves
+	 * 
+	 * @param board       current game board
+	 * @param startSquare the position of the piece
+	 * @return ArrayList of Move objects
+	 */
+	public static List<Move> generateDiagonalMoves(Board board, int startSquare) {
+		List<Integer> directions = generateStartingDirectionsDiagonal(startSquare);
+		return generateDirectionalMoves(board, startSquare, directions);
+	}
 
+	// Walks in each direction until border of the board or another piece is reached
+	private static List<Move> generateDirectionalMoves(Board board, int startSquare, List<Integer> directions) {
+		int teamColor = Piece.getColor(board.getPieceAt(startSquare));
+		int opponentColor = (teamColor == Piece.White) ? Piece.Black : Piece.White;
+		List<Move> generatedMoves = new ArrayList<>();
+		int currentSquare;
+		int piece;
+		for (int direction : directions) {
+			currentSquare = startSquare;
+			do {
+				currentSquare += direction;
+				piece = board.getPieceAt(currentSquare);
+				if (Piece.isType(piece, Piece.None) || Piece.isColor(piece, opponentColor)) {
+					generatedMoves.add(new Move(startSquare, currentSquare));
+					if (Piece.isColor(piece, opponentColor))
+						break;
+				}
+				if (Piece.isColor(piece, teamColor))
+					break;
+			} while (!Coordinate.isOnBorderTowards(currentSquare, direction));
+		}
+		return generatedMoves;
+	}
 
-    /**
-     * Generate diagonal moves
-     * @param startSquare the position of the piece
-     * @return ArrayList of Move objects
-     */
-    public static List<Move> generateDiagonalMoves(Board board, int startSquare) {
-        List<Integer> directions = generateStartingDirectionsDiagonal(startSquare);
-        return generateDirectionalMoves(board, startSquare, directions);
-    }
+	/**
+	 * Make sure to not walk off the board
+	 * 
+	 * @param startSquare position of the piece
+	 * @return returns suitable directions
+	 */
+	public static List<Integer> generateStartingDirectionsAcross(int startSquare) {
+		List<Integer> directions = new ArrayList<>();
+		if (!Coordinate.isOnLeftBorder(startSquare))
+			directions.add(MoveGenerator.LEFT);
+		if (!Coordinate.isOnRightBorder(startSquare))
+			directions.add(MoveGenerator.RIGHT);
+		if (!Coordinate.isOnUpperBorder(startSquare))
+			directions.add(MoveGenerator.UP);
+		if (!Coordinate.isOnLowerBorder(startSquare))
+			directions.add(MoveGenerator.DOWN);
+		return directions;
+	}
 
-
-    /**
-     * Walks in each direction until border of the board or another piece is reached
-     * @param startSquare the position of the piece
-     * @param directions ArrayList of {UP, DOWNRIGHT, ...}
-     * @return ArrayList of Move objects
-     */
-    private static List<Move> generateDirectionalMoves(Board board, int startSquare, List<Integer> directions) {
-        int teamColor = Piece.getColor(board.getPieceAt(startSquare));
-        int opponentColor = (teamColor == Piece.White) ? Piece.Black : Piece.White;
-        List<Move> generatedMoves = new ArrayList<>();
-        int currentSquare;
-        int piece;
-        for (int direction : directions) {
-            currentSquare = startSquare;
-            do {
-                currentSquare += direction;
-                piece = board.getPieceAt(currentSquare);
-                if (Piece.isType(piece, Piece.None) || Piece.isColor(piece, opponentColor)) {
-                    generatedMoves.add(new Move(startSquare, currentSquare));
-                    if (Piece.isColor(piece, opponentColor))
-                        break;
-                }
-                if (Piece.isColor(piece, teamColor))
-                    break;
-            } while (!Coordinate.isOnBorderTowards(currentSquare, direction));
-        }
-        return generatedMoves;
-    }
-
-
-    /**
-     * Make sure to not walk off the board
-     * @param startSquare position of the piece
-     * @return returns suitable directions
-     */
-    public static List<Integer> generateStartingDirectionsAcross(int startSquare) {
-        List<Integer> directions = new ArrayList<>();
-        if (!Coordinate.isOnLeftBorder(startSquare))
-            directions.add(MoveGenerator.LEFT);
-        if (!Coordinate.isOnRightBorder(startSquare))
-            directions.add(MoveGenerator.RIGHT);
-        if (!Coordinate.isOnUpperBorder(startSquare))
-            directions.add(MoveGenerator.UP);
-        if (!Coordinate.isOnLowerBorder(startSquare))
-            directions.add(MoveGenerator.DOWN);
-        return directions;
-    }
-
-
-    /**
-     * Make sure to not walk off the board
-     * @param startSquare position of the piece
-     * @return returns suitable directions
-     */
-    public static List<Integer> generateStartingDirectionsDiagonal(int startSquare) {
-        List<Integer> directions = new ArrayList<>();
-        if (!Coordinate.isOnLeftBorder(startSquare) && !Coordinate.isOnUpperBorder(startSquare))
-            directions.add(MoveGenerator.UPLEFT);
-        if (!Coordinate.isOnRightBorder(startSquare) && !Coordinate.isOnUpperBorder(startSquare))
-            directions.add(MoveGenerator.UPRIGHT);
-        if (!Coordinate.isOnLeftBorder(startSquare) && !Coordinate.isOnLowerBorder(startSquare))
-            directions.add(MoveGenerator.DOWNLEFT);
-        if (!Coordinate.isOnRightBorder(startSquare) && !Coordinate.isOnLowerBorder(startSquare))
-            directions.add(MoveGenerator.DOWNRIGHT);
-        return directions;
-    }
+	/**
+	 * Make sure to not walk off the board
+	 * 
+	 * @param startSquare position of the piece
+	 * @return returns suitable directions
+	 */
+	public static List<Integer> generateStartingDirectionsDiagonal(int startSquare) {
+		List<Integer> directions = new ArrayList<>();
+		if (!Coordinate.isOnLeftBorder(startSquare) && !Coordinate.isOnUpperBorder(startSquare))
+			directions.add(MoveGenerator.UPLEFT);
+		if (!Coordinate.isOnRightBorder(startSquare) && !Coordinate.isOnUpperBorder(startSquare))
+			directions.add(MoveGenerator.UPRIGHT);
+		if (!Coordinate.isOnLeftBorder(startSquare) && !Coordinate.isOnLowerBorder(startSquare))
+			directions.add(MoveGenerator.DOWNLEFT);
+		if (!Coordinate.isOnRightBorder(startSquare) && !Coordinate.isOnLowerBorder(startSquare))
+			directions.add(MoveGenerator.DOWNRIGHT);
+		return directions;
+	}
 }
--- src/main/java/chess/model/MoveValidator.java
+++ src/main/java/chess/model/MoveValidator.java
@@ -1,6 +1,5 @@
 package chess.model;
 
-
 import java.util.List;
 
 /**
@@ -8,121 +7,115 @@ import java.util.List;
  */
 public class MoveValidator {
 
-    private static int findKing(Board board, int color) {
-        List<Integer> positions = board.getPiecePositionsFor(color);
-        for (int squarePosition : positions) {
-            if (Piece.isType(board.getPieceAt(squarePosition), Piece.King))
-                return squarePosition;
-        }
-        return -1;
-    }
+	private static int findKing(Board board, int color) {
+		List<Integer> positions = board.getPiecePositionsFor(color);
+		for (int squarePosition : positions) {
+			if (Piece.isType(board.getPieceAt(squarePosition), Piece.King))
+				return squarePosition;
+		}
+		return -1;
+	}
 
+	/**
+	 * Filters a list of moves
+	 * 
+	 * @param board the current gme board
+	 * @param moves a list of moves
+	 * @return a list of valid moves
+	 */
+	public static List<Move> filter(Board board, List<Move> moves) {
+		moves.removeIf(m -> !validateMove(board, m));
+		return moves;
+	}
 
-    /**
-     * Filters a list of moves
-     * @param moves a list of moves
-     * @return a list of valid moves
-     */
-    public static List<Move> filter(Board board, List<Move> moves) {
-        moves.removeIf(m -> !validateMove(board, m));
-        return moves;
-    }
+	/**
+	 * Checks for check
+	 * 
+	 * @param board     the current position
+	 * @param teamColor which piece color to check
+	 * @return 0: no check, 1: check, 2: double check
+	 */
+	public static int checkCheck(Board board, int teamColor) {
+		int numChecks = 0;
+		MoveGenerator generator = new MoveGenerator(board);
+		int kingPosition = findKing(board, teamColor);
 
+		// swap color to opponent
+		if (generator.getTeamColor() == teamColor)
+			generator.swapColors();
 
-    /**
-     * Checks for check
-     * @param board the current position
-     * @param teamColor which piece color to check
-     * @return 0: no check, 1: check, 2: double check
-     */
-    public static int checkCheck(Board board, int teamColor) {
-        int numChecks = 0;
-        MoveGenerator generator = new MoveGenerator(board);
-        int kingPosition = findKing(board, teamColor);
+		// find opponent moves and count if king is in check
+		for (Move move : generator.generateMoves()) {
+			if (move.getTargetSquare() == kingPosition) {
+				numChecks += 1;
+				if (numChecks == 2)
+					return numChecks;
+			}
+		}
+		return numChecks;
+	}
 
-        // swap color to opponent
-        if (generator.getTeamColor() == teamColor)
-            generator.swapColors();
+	// Check if move is any legal move found by the MoveGenerator
+	private static boolean isLegalMove(MoveGenerator generator, Move move) {
+		return generator.generateMovesStartingAt(move.getStartSquare()).contains(move);
+	}
 
-        // find opponent moves and count if king is in check
-        for (Move move : generator.generateMoves()) {
-            if (move.getTargetSquare() == kingPosition) {
-                numChecks += 1;
-                if (numChecks == 2)
-                    return numChecks;
-            }
-        }
-        return numChecks;
-    }
+	// if king is in check or any square between the king and the pawn is under
+	// attack he cannot castle
+	private static boolean canCastle(MoveGenerator generator, Move move, int inCheck) {
+		if (0 < inCheck)
+			return false;
+		generator.swapColors();
+		List<Move> opponentMoves = generator.generateMoves();
+		// check left side
+		if (move.getTargetSquare() < move.getStartSquare()) {
+			for (int i = 1; i < 4; i++) {
+				if (isTarget(move.getStartSquare() - i, opponentMoves))
+					return false;
+			}
+		} else {
+			// check right side
+			for (int i = 1; i < 3; i++) {
+				if (isTarget(move.getStartSquare() + i, opponentMoves))
+					return false;
+			}
+		}
+		return true;
+	}
 
+	/**
+	 * Checks if a move is legal
+	 * 
+	 * @param board the current position
+	 * @param move  what to check
+	 * @return true if the move is legal
+	 */
+	public static boolean validateMove(Board board, Move move) {
+		MoveGenerator generator = new MoveGenerator(board);
+		if (!isLegalMove(generator, move))
+			return false;
 
-    // check if move is any legal move found by the MoveGenerator
-    private static boolean isLegalMove(MoveGenerator generator, Move move) {
-        return generator.generateMovesStartingAt(move.getStartSquare()).contains(move);
-    }
+		Board boardAfterMove = board.makeMove(move);
 
+		int inCheck = checkCheck(board, board.getTurnColor());
+		// check if king is inCheck and tries to castle
+		if (0 < inCheck && move.getFlag() == Move.Castling) {
+			return false;
+		}
 
-    // if king is in check or any square between the king and the pawn is under attack he cannot castle
-    private static boolean canCastle(MoveGenerator generator, Move move, int inCheck) {
-            if (0 < inCheck)
-                return false;
-            generator.swapColors();
-            List<Move> opponentMoves = generator.generateMoves();
-            // check left side
-            if (move.getTargetSquare() < move.getStartSquare()) {
-                for (int i=1;i<4;i++) {
-                    if (isTarget(move.getStartSquare() - i, opponentMoves))
-                        return false;
-                }
-            } else {
-                // check right side
-                for (int i=1;i<3;i++) {
-                    if (isTarget(move.getStartSquare() + i, opponentMoves))
-                        return false;
-                }
-            }
-        return true;
-    }
+		// check if move puts own king in chess
+		if (0 < checkCheck(boardAfterMove, board.getTurnColor()))
+			return false;
 
+		return move.getFlag() != Move.Castling || canCastle(generator, move, inCheck);
+	}
 
-    /**
-     * Checks if a move is legal
-     * @param board the current position
-     * @param move what to check
-     * @return true if the move is legal
-     */
-    public static boolean validateMove(Board board, Move move) {
-        MoveGenerator generator = new MoveGenerator(board);
-        if (!isLegalMove(generator, move))
-            return false;
-
-        Board boardAfterMove = board.makeMove(move);
-
-        int inCheck = checkCheck(board, board.getTurnColor());
-        // check if king is inCheck and tries to castle
-        if (0 < inCheck && move.getFlag() == Move.Castling) {
-            return false;
-        }
-
-        // check if move puts own king in chess
-        if (0 < checkCheck(boardAfterMove, board.getTurnColor()))
-            return false;
-
-        return move.getFlag() != Move.Castling || canCastle(generator, move, inCheck);
-    }
-
-
-    /**
-     * Checks if squarePosition is a targetSquare of any move in moves
-     * @param squarePosition what position to check
-     * @param moves ArrayList of moves to check
-     * @return true if any move contains squarePosition as target
-     */
-    private static boolean isTarget(int squarePosition, List<Move> moves) {
-        for (Move m : moves) {
-            if (m.getTargetSquare() == squarePosition)
-                return true;
-        }
-        return false;
-    }
+	// Checks if squarePosition is a targetSquare of any move in moves
+	private static boolean isTarget(int squarePosition, List<Move> moves) {
+		for (Move m : moves) {
+			if (m.getTargetSquare() == squarePosition)
+				return true;
+		}
+		return false;
+	}
 }
