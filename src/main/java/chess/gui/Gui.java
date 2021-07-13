f90354c2ced9260950f45192f943e95edad1a1f1
Miklos Libak <miklos.libak@student.uni-luebeck.de>
2021-07-13 11:32:06 +0200

    Add app icon

--- src/main/java/chess/gui/Gui.java
+++ src/main/java/chess/gui/Gui.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -23,21 +24,24 @@ public class Gui extends Application {
 	/**
 	 * This method is called by the Application to start the GUI.
 	 *
-	 * @param primaryStage The initial root stage of the application.
+	 * @param stage The initial root stage of the application.
 	 */
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage stage) {
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("MenuView.fxml"));
 			scene = new Scene(root);
 
+			// set icon of the application
+			stage.getIcons().add(new Image(Gui.class.getResourceAsStream("/chess/gui/images/icons/logo.png")));
+
 			// activate the menuScene (MenuView) as default entry scene
-			primaryStage.setScene(scene);
-			primaryStage.show();
-			double titleBarHeight = primaryStage.getHeight() - primaryStage.getScene().getHeight();
-			primaryStage.setMinWidth(640);
-			primaryStage.setMinHeight(360 + titleBarHeight);
-			primaryStage.setTitle("Chess");
+			stage.setScene(scene);
+			stage.show();
+			double titleBarHeight = stage.getHeight() - stage.getScene().getHeight();
+			stage.setMinWidth(640);
+			stage.setMinHeight(360 + titleBarHeight);
+			stage.setTitle("Chess");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
